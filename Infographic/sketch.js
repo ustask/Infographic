b101a364c9236ec032dd17491def1c5f54eb8653
Francisco Dias <frandias@gmail.com>
2016-02-16 19:55:38 -0500

    made an infographic based on word length

--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,11 +1,20 @@
-console.log(baconIpsum.length)
-
 var baconWords = baconIpsum.split(' ');
-
-console.log(baconWords)
+for (var i = 0; i < baconWords.length; i++) {
+  baconWords[i] = baconWords[i].length;
+}
 
 function setup() {
+  createCanvas(600,600)
 
+  for (var i = 0; i < baconWords.length; i++){
+    var rectHeight = baconWords[i] * 60;
+    var rectWidth = (width / baconWords.length);
+    console.log(rectWidth)
+
+    fill('#f338de');
+    noStroke()
+    rect(i * 10, width - rectHeight, rectWidth, rectHeight);
+  }
 }
 
 function draw() {
