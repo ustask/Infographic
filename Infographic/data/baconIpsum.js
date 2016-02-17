4ea7022c1386ad9b317346e3111dd123e610b4c1
Francisco Dias <frandias@gmail.com>
2016-02-16 19:31:29 -0500

    we have our baconIpsum split up

---
+++ Infographic/data/baconIpsum.js
@@ -0,0 +1,1 @@
+var baconIpsum = "Bacon ipsum dolor amet strip steak hamburger cow bacon shank. Capicola doner tail meatloaf salami turducken shank, prosciutto jerky pork ham porchetta venison. Prosciutto alcatra cupim tri-tip brisket short loin. Rump porchetta chuck, ground round turkey brisket t-bone frankfurter prosciutto shoulder pork chop spare ribs jerky jowl drumstick.";
--- Infographic/sketch.js
+++ Infographic/sketch.js
@@ -1,10 +1,13 @@
-var baconIpsum = "Bacon ipsum dolor amet strip steak hamburger cow bacon shank. Capicola doner tail meatloaf salami turducken shank, prosciutto jerky pork ham porchetta venison. Prosciutto alcatra cupim tri-tip brisket short loin. Rump porchetta chuck, ground round turkey brisket t-bone frankfurter prosciutto shoulder pork chop spare ribs jerky jowl drumstick.";
-console.log(baconIpsum)
+console.log(baconIpsum.length)
+
+var baconWords = baconIpsum.split(' ');
+
+console.log(baconWords)
 
 function setup() {
-  
+
 }
 
 function draw() {
-  
+
 }
